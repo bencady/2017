dedff5ae7fcca66aa85cdf03bf2f81fab79f12d8
Ben Cady <bencady@seattleacademy.org>
2020-02-05 11:13:01 -0800

    create new files

--- index.js
+++ index.js
@@ -1,2 +1,27 @@
-console.log("hello");
+console.log("Hello World");
 
+const express = require('express')
+const app = express()
+const port = 1517
+
+app.get('/hello', (req, res) => res.send('Hello JOSH!! WE ARE WATCHING YOU!! LOOK BEHIND YOU'))
+
+app.use(express.static('public'));
+
+
+app.get('/russia', function (req, res) {
+	counter = counter + 1;
+  res.send('Bye Moscow' + counter)
+})
+
+
+
+app.get('/Moscow', function (req, res) {
+  res.send('3... 2... 1... Boom')
+})
+app.get('/date', function (req, res) {
+	var d = Date();
+  res.send(d)
+})
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
